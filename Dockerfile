42f4354028930911902c11a5accb8926972cd7f8
Chris Conner <cconner@cloudera.com>
2018-04-07 12:03:35 -0400

    First commit fully functional

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM ubuntu:artful
+MAINTAINER chrism.conner@gmail.com
+
+ENV BIND_USER=bind \
+    BIND_VERSION=1:9.10.3 \
+    WEBMIN_VERSION=1.8 \
+    DATA_DIR=/data \
+    INTERFACES= \
+    WEBMIN_ENABLED=true
+
+RUN rm -rf /etc/apt/apt.conf.d/docker-gzip-indexes \
+ && apt-get update \
+ && apt-get install -y rsyslog supervisor nano vim zsh wget dnsutils lnav \
+ && wget http://www.webmin.com/jcameron-key.asc -qO - | apt-key add - \
+ && echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list \
+ && apt-get update \
+ && DEBIAN_FRONTEND=noninteractive apt-get install -y bind9=${BIND_VERSION}* bind9-host=${BIND_VERSION}* webmin=${WEBMIN_VERSION}* isc-dhcp-server \
+ && rm -rf /var/lib/apt/lists/*
+
+COPY dhcpd.conf.exemple /etc/dhcp/dhcpd.conf
+COPY supervisord.conf /etc/supervisord.conf
+COPY entrypoint.sh /sbin/entrypoint.sh
+RUN chmod 755 /sbin/entrypoint.sh
+
+RUN touch /var/log/syslog
+RUN chown syslog:adm /var/log/syslog
+
+VOLUME ["${DATA_DIR}"]
+EXPOSE 53/udp 53/tcp 547/udp 547/tcp 10000/tcp
+ENTRYPOINT ["/sbin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#set -e
+set -x
+
+ROOT_PASSWORD=${ROOT_PASSWORD:-password}
+BIND_DATA_DIR=${DATA_DIR}/bind
+DHCP_DATA_DIR=${DATA_DIR}/dhcp
+WEBMIN_DATA_DIR=${DATA_DIR}/webmin
+
+for SRV_DIR in "bind" "dhcp" "webmin"
+do
+  mkdir -p ${DATA_DIR}/${SRV_DIR}
+
+  # populate default configuration if it does not exist
+  if [ ! -d ${DATA_DIR}/${SRV_DIR}/etc ]; then
+    mv /etc/${SRV_DIR} ${DATA_DIR}/${SRV_DIR}/etc
+  fi
+  rm -rf /etc/${SRV_DIR}
+  ln -sf ${DATA_DIR}/${SRV_DIR}/etc /etc/${SRV_DIR}
+  echo "Create links for " ${SRV_DIR}
+  chmod -R 0775 ${DATA_DIR}/${SRV_DIR}
+  
+  # bind config
+  if [ ${SRV_DIR} = bind ]; then 
+    chown -R ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}
+    if [ ! -d ${BIND_DATA_DIR}/lib ]; then
+      mkdir -p ${BIND_DATA_DIR}/lib
+      chown ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}/lib
+    fi
+    rm -rf /var/lib/bind
+    ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
+	  
+    mkdir -m 0775 -p /var/run/named
+    mkdir -m 0775 -p /var/cache/bind
+    chown root:${BIND_USER} /var/run/named
+    chown root:${BIND_USER} /var/cache/bind
+  fi
+
+  if [ ${SRV_DIR} = webmin ]; then
+    if [ ! "${WEBMIN_ENABLED}" == "true" ]; then
+      mv ${DATA_DIR}/${SRV_DIR} ${DATA_DIR}/${SRV_DIR}.moved
+    else
+      ln -sf ${WEBMIN_DATA_DIR}/etc /etc/webmin
+    fi
+  fi
+  
+  # dhcp config
+  if [ ${SRV_DIR} = dhcp ]; then
+    if [ ! -d ${DHCP_DATA_DIR}/lib ]; then
+      mkdir -p ${DHCP_DATA_DIR}/lib
+    fi
+    rm -rf /var/lib/dhcp
+    ln -sf ${DHCP_DATA_DIR}/lib /var/lib/dhcp
+    chown -R root:dhcpd ${DHCP_DATA_DIR}/lib
+    touch ${DHCP_DATA_DIR}/lib/dhcp/dhcpd.leases
+    touch /var/run/dhcpd.pid
+    chown root:dhcpd ${DHCP_DATA_DIR}/lib/dhcp/dhcpd.leases
+    IP="`ifconfig $INTERFACES | awk '/inet addr/{print substr($2,6)}'`"
+    MASK="`ifconfig $INTERFACES | grep Mask | cut -d":" -f4`"
+    sed -i '/INTERFACES*/c\INTERFACES="'$INTERFACES'"' /etc/default/isc-dhcp-server
+  fi
+done
+
+set_root_passwd() {
+  echo "root:$ROOT_PASSWORD" | chpasswd
+}
+
+set_root_passwd
+echo "Starting webmin..."
+/etc/init.d/webmin start
+
+sed -i "/autostart=false/c\autostart=true" /etc/supervisord.conf
+
+NAMED_ORIG="command=NAMED"
+NAMED="command=/usr/sbin/named -4 -c /etc/bind/named.conf -f -u bind"
+sed -i "s|$NAMED_ORIG|$NAMED|g" /etc/supervisord.conf
+
+#  DHCPD="command=/usr/sbin/dhcpd "$INTERFACES" -pf /var/run/dhcpd.pid -f"
+DHCPD_ORIG="command=DHCPD"
+DHCPD="command=/usr/sbin/dhcpd -user dhcpd -group dhcpd -f -4 -pf /var/run/dhcpd.pid -cf /etc/dhcp/dhcpd.conf"
+sed -i "s|$DHCPD_ORIG|$DHCPD|g" /etc/supervisord.conf
+
+exec /usr/bin/supervisord -c /etc/supervisord.conf
